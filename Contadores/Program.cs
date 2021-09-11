38adeb26bd3e520b342ae9c2c6160b93cbcbbecb
Jorge Alberto Cricelli <jacricelli@gmail.com>
2021-09-11 08:04:37 -0300

    Add project files.

---
+++ Contadores/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Contadores
+{
+    /// <summary>
+    /// Programa.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        /// <param name="args">Los argumentos de la línea de comandos.</param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Contadores/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Contadores")]
+[assembly: AssemblyDescription("Recopila y almacena los contadores de los usuarios de impresoras Ricoh.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Auditore")]
+[assembly: AssemblyCopyright("Copyright © 2021 Jorge Alberto Cricelli")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4cbbc9ad-3589-4293-8e28-2d187c0327b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
